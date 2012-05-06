a6c0451c52846c6c934381863f4debd96d6f0fe2
Kasper Isager <kasperisager@gmail.com>
2012-05-06 18:21:26 +0200

    Initial support for Bootswatches

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 $ThemeInfo['Bootstrap'] = array(
    'Name' => 'Bootstrap',
    'Description' => "Based on Bootstrap from Twitter and inspired by LiveRoom.<br>Be sure to read the documentation found here: <a href='https://github.com/kasperisager/VanillaBootstrap'>https://github.com/kasperisager/VanillaBootstrap</a>",
-   'Version' => '1.0b-2.0',
+   'Version' => '1.1b-2.0',
    'Author' => "Kasper K. Isager",
    'AuthorEmail' => 'kasperisager@gmail.com',
    'AuthorUrl' => 'https://github.com/kasperisager'
