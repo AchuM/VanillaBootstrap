59172a5c0a542d9f53ef39ea663dfc8b359e4781
Kasper Isager <kasperisager@gmail.com>
2012-05-18 18:29:35 +0200

    Responsiveness and LESS fixes
    Fixed responsiveness, changed a few styles and updated the description
    to match the new version

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <?php if (!defined('APPLICATION')) exit();
 $ThemeInfo['Bootstrap'] = array(
    'Name' => 'Bootstrap',
-   'Description' => "Based on Bootstrap from Twitter and inspired by LiveRoom.<br>Be sure to read the documentation found here: <a href='https://github.com/kasperisager/VanillaBootstrap'>https://github.com/kasperisager/VanillaBootstrap</a>",
+   'Description' => "Based on Bootstrap from Twitter and inspired by the Twitter dev site.<br>Be sure to read the documentation found here: <a href='https://github.com/kasperisager/VanillaBootstrap'>https://github.com/kasperisager/VanillaBootstrap</a>",
    'Version' => '2.0a-2.1',
    'Author' => "Kasper K. Isager",
    'AuthorEmail' => 'kasperisager@gmail.com',
